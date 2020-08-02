c347530c8aaf27c230223dcfec59ad4a8e512409
Ikhlas Ahmed <ike.ahmed@aa.com>
2020-08-02 17:03:15 -0500

    updates

---
+++ day2/twonumbers.py
@@ -0,0 +1,8 @@
+print ('Enter first number: ')
+num1=int(input())
+print('Enter second number: ')
+num2=int(input())
+result = num1+num2 
+print('sum of two numbers is ')
+print (result)
+# Ask students to do subtraction, multiplication and division & discuss Division "/" , Floor division "//" and Modulus "%"
---
+++ day2/yourname.py
@@ -0,0 +1,3 @@
+#Use the following code snippets to introduce the concepts of input/output,
+name=input("What is your name? ")
+print("Hello " + name)
