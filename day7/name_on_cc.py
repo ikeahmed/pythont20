061c060e11cab833589d68e1018e09c21c71590e
Ikhlas Ahmed <ike.ahmed@aa.com>
2020-08-30 17:01:17 -0500

    updates

---
+++ day7/name_on_cc.py
@@ -0,0 +1,8 @@
+fName = input('Enter first name: ')
+mName = input('Enter middle name: ')
+lName = input('Enter last name: ')
+
+def fullName(fName, mName, lName):
+  return (fName + " " + mName + " " + lName).upper()
+
+print (fullName('Ik','ah','mm'))
